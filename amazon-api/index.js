9eb86dae5c5b9bc0f235d8b6fdff7c2228a62822
Tesfamichael Tafere <tesfamichael132@gmail.com>
2025-07-03 00:40:56 +0300

    style: Update CORS configuration to allow multiple origins

--- amazon-api/index.js
+++ amazon-api/index.js
@@ -10,7 +10,10 @@ const ordersApi = require("./orders");
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://amazon-clone-frontend-phi.vercel.app"
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
