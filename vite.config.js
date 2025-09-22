06730ac5b3de3b265405fd857853b3ae03d8244c
Reddy-1227 <gsr984945@gmail.com>
2025-09-22 23:25:45 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { Buffer } from 'buffer';
+import crypto from 'crypto';
 
-// https://vite.dev/config/
+if (!globalThis.crypto) {
+  globalThis.crypto = crypto.webcrypto;
+}
+
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    sourcemap: true, // Enable source maps for production
+  define: {
+    'process.env': {}
+  },
+  resolve: {
+    alias: {
+      crypto: 'crypto-browserify',
+    },
   },
 });
+
